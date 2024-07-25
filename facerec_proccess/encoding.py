f146468ccd29aacf47fe34c14d550270ac1d07ce
rocery <sastranuralamsyah@gmail.com>
2024-07-25 13:21:28 +0700

    add encoding that finally we use

--- facerec_proccess/encoding.py
+++ facerec_proccess/encoding.py
@@ -1,38 +1,148 @@
-import face_recognition
+import math
+from sklearn import neighbors
 import os
+import os.path
 import pickle
+from PIL import Image, ImageDraw
+import face_recognition
+from face_recognition.face_recognition_cli import image_files_in_folder
+import csv
+import time
+import shutil
+from datetime import datetime
 
-def encode_faces_from_train_folder():
-    # Path to the train folder inside the encoding folder
-    train_folder_path = 'encoding/train'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'JPG'}
+
+train_folder = "uploads/train"
+fail_folder = "uploads/fail"
+model_save_path = "static/clf/trained_knn_model.clf"
+csv_success = 'uploads/success_trained.csv'
+csv_fail = 'uploads/fail_trained.csv'
+
+def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree', verbose=True):
+    """
+    Trains a k-nearest neighbors classifier for face recognition.
+
+    :param train_dir: directory that contains a sub-directory for each known person, with its name.
+
+    (View in source code to see train_dir example tree structure)
+
+    Structure:
+        <train_dir>/
+        ├── <person1>/
+        │   ├── <somename1>.jpeg
+        │   ├── <somename2>.jpeg
+        │   ├── ...
+        ├── <person2>/
+        │   ├── <somename1>.jpeg
+        │   └── <somename2>.jpeg
+        └── ...
+
+    :param model_save_path: (optional) path to save model on disk
+    :param n_neighbors: (optional) number of neighbors to weigh in classification. Chosen automatically if not specified
+    :param knn_algo: (optional) underlying data structure to support knn.default is ball_tree
+    :param verbose: verbosity of training
+    :return: returns knn classifier that was trained on the given data.
+    """
+    X = []
+    y = []
     
-    # Dictionary to hold the encodings
-    face_encodings = {}
+    folder_counter = 0
+    img_counter = 0
+    failed_images_counter = 0
+    total_time_encoding = 0
+
+    # Loop through each person in the training set
+    for class_dir in os.listdir(train_dir):
+        if not os.path.isdir(os.path.join(train_dir, class_dir)):
+            continue
+
+        folder_counter += 1
+        # Loop through each training image for the current person
+        for img_path in image_files_in_folder(os.path.join(train_dir, class_dir)):
+            image = face_recognition.load_image_file(img_path)
+            face_bounding_boxes = face_recognition.face_locations(image)
+            img_counter += 1
+            
+            start_time_encoding = time.time()
+            
+            if len(face_bounding_boxes) != 1:
+                # If there are no people (or too many people) in a training image, skip the image.
+                if verbose:
+                    print("Image {} not suitable for training: {}".format(img_path, "Didn't find a face" if len(face_bounding_boxes) < 1 else "Found more than one face"))
+                    failed_images_counter += 1
+                    
+                    with open (csv_fail, mode='a', newline='') as file:
+                        writer = csv.writer(file)
+                        now = datetime.now()
+                        writer.writerow([img_counter, class_dir, img_path, "Wajah tidak terdeteksi" if len(face_bounding_boxes) < 1 else "Wajah terdeteksi lebih dari 1", now.strftime("%Y-%m-%d %H:%M:%S")])
+                shutil.move(img_path, os.path.join("uploads/fail", os.path.basename(img_path)))
+                
+            else:
+                # Add face encoding for current image to the training set
+                X.append(face_recognition.face_encodings(image, known_face_locations=face_bounding_boxes)[0])
+                y.append(class_dir)
+                
+                end_time_encoding = time.time()
+                time_encoding = end_time_encoding - start_time_encoding
+                total_time_encoding += time_encoding
+                
+                print(f"{folder_counter}. {class_dir}:", 
+                    f"File {img_counter}. {img_path} diproses. Waktu Encoding: {time_encoding:.2f} detik")
+                
+                with open (csv_success, mode='a', newline='') as file:
+                    writer = csv.writer(file)
+                    now = datetime.now()
+                    writer.writerow([img_counter, class_dir, img_path, now.strftime("%Y-%m-%d %H:%M:%S")])
+                
+    # Determine how many neighbors to use for weighting in the KNN classifier
+    if n_neighbors is None:
+        n_neighbors = int(round(math.sqrt(len(X))))
+        if verbose:
+            print("Chose n_neighbors automatically:", n_neighbors)
+
+    # Create and train the KNN classifier
+    knn_clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, algorithm=knn_algo, weights='distance')
+    knn_clf.fit(X, y)
+
+    # Save the trained KNN classifier
+    if model_save_path is not None:
+        with open(model_save_path, 'wb') as f:
+            pickle.dump(knn_clf, f)
+            
+    print(f"Folder diproses: {folder_counter}")
+    print(f"Gambar diproses: {img_counter}")
+    print(f"Total Waktu Encoding: {total_time_encoding:.2f} detik")
+    print(f"Total Waktu Encoding: {total_time_encoding // 60:.0f} menit {total_time_encoding % 60:.0f} detik")
+    print(f"Rata-Rata Waktu Encoding per Gambar: {total_time_encoding / img_counter:.2f} detik")
+    print(f"Gambar yang gagal diproses: {failed_images_counter}")
+
+    return knn_clf
+
+if __name__ == "__main__":
+    # Jika model ada, hapus dulu
+    if os.path.exists(model_save_path):
+        os.remove(model_save_path)
     
-    # Iterate over each file in the train folder
-    for filename in os.listdir(train_folder_path):
-        print(filename)
-#         if filename.endswith('.jpg') or filename.endswith('.png'):
-#             # Construct full file path
-#             file_path = os.path.join(train_folder_path, filename)
-#             print(f"Processing file: {filename}")  # Status update for current file in encoding progress
-#             # Load the image file
-#             image = face_recognition.load_image_file(file_path)
-#             # Find face encodings
-#             encodings = face_recognition.face_encodings(image)
-#             if encodings:
-#                 # Assuming each image has one face for simplicity
-#                 face_encodings[filename] = encodings[0]
+    # Membuat folder trained jika belum ada
+    if not os.path.exists(fail_folder):
+        os.makedirs(fail_folder)
+        
+    # Membuat CSV jika belum ada
+    if os.path.exists(csv_success):
+        os.remove(csv_success)
+        with open(csv_success, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['No.', 'Folder', 'File', 'Time'])
+    # Membuat CSV jika belum ada
+    if os.path.exists(csv_fail):
+        os.remove(csv_fail)
+        with open(csv_fail, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['No.', 'Folder', 'File', 'Information', 'Time'])
     
-#     # Save the encodings to a file for later use
-#     with open('encoded_faces.pkl', 'wb') as f:
-#         pickle.dump(face_encodings, f)
-
-#     return face_encodings
-
-# # Example usage
-# encoded_faces = encode_faces_from_train_folder()
-# print("Encoded faces saved to file.")
-
-
-
+    print("Training KNN classifier...")
+    classifier = train(train_dir = train_folder,
+                    model_save_path = model_save_path,
+                    n_neighbors = 2)
+    print("Training complete!")
