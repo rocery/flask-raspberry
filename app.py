608a4ff4a97771367a391a54c73918c715072f93
rocery <sastranuralamsyah@gmail.com>
2024-08-23 11:22:44 +0700

    change port

--- app.py
+++ app.py
@@ -323,4 +323,4 @@ if __name__ == '__main__':
     # finally:
     #     GPIO.cleanup
         
-    app.run(host='0.0.0.0', port=5001, threaded=True)
+    app.run(host='0.0.0.0', port=5000, threaded=True)
